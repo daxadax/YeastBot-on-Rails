b6be6ac4a5737cb458daf822d86d89b21c1546de
daxdefranco <daxdefranco+git@gmail.com>
2013-11-06 14:30:20 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YeastBot::Application.config.secret_key_base = '9ab8cb123be88481fe4e428c9834c76396f37feed5febddf96d79626f0d042fb95f6a57d15043d95eae839c34b2edafab20e3f48ffffd60c8ad5388b5df91830'
